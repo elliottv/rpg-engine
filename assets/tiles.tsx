9f70a3e1e0b1f54fb06049a88b67b63b7171489b
elliottv <elliott.vettraino@gmail.com>
2026-08-13 17:41:39 +0200

    Story 6: committed fixture map, tileset and tileset image

---
+++ assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="rpg_fixture_tiles" tilewidth="48" tileheight="48" tilecount="4" columns="2">
+  <image source="tiles.png" width="96" height="96"/>
+</tileset>
